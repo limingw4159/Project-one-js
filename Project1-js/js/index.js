5aa5a1dfcda292d98a95b03358b82ba7d104966d
Joe NOei <limingw4159@gmail.com>
2021-04-20 20:20:35 +1000

    make array for progressbar

--- Project1-js/js/index.js
+++ Project1-js/js/index.js
@@ -6,7 +6,6 @@ class Tab {
   constructor(tabName, mainName) {
     this.tab = document.querySelector(tabName);
     this.home = document.querySelectorAll(mainName);
-
     this.lis = this.tab.querySelectorAll("li");
     this.init();
   }
@@ -32,29 +31,11 @@ class Tab {
     }
   }
   progBar(index, that) {
+    let arr = [80, 90, 70, 90, 80, 95];
     if (index == 1) {
       that.progress = that.home[index].querySelectorAll(".progressbar");
       for (let i = 0; i < that.progress.length; i++) {
-        switch (i) {
-          case 0:
-            that.animate(that.progress[i], 80);
-            break;
-          case 1:
-            that.animate(that.progress[i], 90);
-            break;
-          case 2:
-            that.animate(that.progress[i], 70);
-            break;
-          case 3:
-            that.animate(that.progress[i], 90);
-            break;
-          case 4:
-            that.animate(that.progress[i], 80);
-            break;
-          case 5:
-            that.animate(that.progress[i], 95);
-            break;
-        }
+        that.animate(that.progress[i], arr[i]);
       }
     } else {
       return false;
